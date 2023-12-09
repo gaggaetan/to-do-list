b9d6f4c7b732cda1f34ed3e0ecc292367e62d489
gaëtan carbonnelle <Gaetan.carbonnelle1@gmail.com>
2023-12-09 20:04:08 +0100

    test3 CI-CD : update to pylint

--- libs/class_db.py
+++ libs/class_db.py
@@ -13,15 +13,21 @@ import threading
 import socket
 
 
-class To_do_list_db:
-    """ Class pour la DB """
+class ToDoListDB:
+    """
+    Class pour la DB
+    """
     def __init__(self, db_file: str):
-        """ Initialise les variables de la DB """
+        """
+        Initialise les variables de la DB
+        """
         self._db_file = db_file #dosier avec la DB
         self.nbr_client = 0 #nombre de client qui sont connectrer à la DB
 
     def delete_all_table_in_db(self):
-        """ Supprimer les 2 tables dans la DB """
+        """
+        Supprimer les 2 tables dans la DB
+        """
 
         conn = None
         try:
@@ -38,7 +44,9 @@ class To_do_list_db:
                 conn.close()
 
     def create_all_table_in_db(self):
-        """ Crée les tables de la DB """
+        """
+        Crée les tables de la DB
+        """
 
         conn = None
         try:
@@ -60,17 +68,21 @@ class To_do_list_db:
             print(e)
         finally:
             if conn:
-                # ferme la connection avec la DB
+                #ferme la connection avec la DB
                 conn.close()
 
     def reset_db(self):
-        """ Reset les tables de la DB """
+        """
+        Reset les tables de la DB
+        """
 
         self.delete_all_table_in_db()
         self.create_all_table_in_db()
 
     def execute_sql_insert(self, query):
-        """ Execute la requete SQL INSERT dans la DB et retourne id de l'auto incrémentation dans la DB """
+        """
+        Execute la requete SQL INSERT dans la DB et retourne id de l'auto incrémentation dans la DB
+        """
 
         conn = None
         try:
@@ -82,7 +94,8 @@ class To_do_list_db:
             #execute le code SQL dans la DB
             cur.execute(query)
 
-            #teste si il y a un id autoincrément avec "cur.lastrowid" pour le retourner apres, aussinon il passe à la suite
+            #teste si il y a un id autoincrément avec "cur.lastrowid" pour le retourner apres,
+            # aussi non il passe à la suite
             try:
                 #trouver le dernier identifiant d'autoincrément
                 last_id_auto_increment = cur.lastrowid
@@ -101,7 +114,9 @@ class To_do_list_db:
                 conn.close()
 
     def execute_sql_select(self, query):
-        """ Execute la requete SQL SELECT dans la DB et retourne le résultat du select dans la DB """
+        """
+        Execute la requete SQL SELECT dans la DB et retourne le résultat du select dans la DB
+        """
 
         conn = None
         try:
@@ -121,11 +136,13 @@ class To_do_list_db:
             return None
         finally:
             if conn:
-                # ferme la connection avec la DB
+                #ferme la connection avec la DB
                 conn.close()
 
     def execute_sql_delete(self, query):
-        """ Execute la requete SQL DELETE dans la DB """
+        """
+        Execute la requete SQL DELETE dans la DB
+        """
 
         conn = None
         try:
@@ -136,30 +153,36 @@ class To_do_list_db:
 
             #execute le code SQL dans la DB
             cur.execute(query)
-            # commit l'instruction SQL
+            #commit l'instruction SQL
             conn.commit()
 
         except Error as e:
             print(e)
         finally:
             if conn:
-                # ferme la connection avec la DB
+                #ferme la connection avec la DB
                 conn.close()
 
     def new_clients(self, nbr_new_clients):
-        """ Crée un certain nombre de nouveau client dans des nouvelle ligne de commande (cmd) """
+        """
+        Crée un certain nombre de nouveau client dans des nouvelle ligne de commande (cmd)
+        """
 
         for _ in range(int(nbr_new_clients)):
 
-            # Chemin absolu vers le script client.py
+            #chemin absolu vers le script client.py
             client_script_path = os.path.abspath("client.py")
 
-            #Lancer un nouveau terminal avec le script client.py qui va créer un nouveau client
-            subprocess.run(["start", "cmd", "/k", "python", client_script_path] + [f"{15600 + self.nbr_client}"],shell=True, check=True)
+            #lancer un nouveau terminal avec le script client.py qui va créer un nouveau client
+            subprocess.run(["start", "cmd", "/k", "python", client_script_path] +
+                           [f"{15600 + self.nbr_client}"],shell=True, check=True)
             self.nbr_client += 1
 
     def update_client_new_task(self):
-        """ Envois une requete à tout les clients pour prévenir  qu'un nouvelle tache à été ajouter à la DB """
+        """
+        Envois une requete à tout les clients pour prévenir  qu'un nouvelle tache à
+        été ajouter à la DB
+        """
 
         def update_client_end_db_thread(index):
             try:
@@ -173,17 +196,20 @@ class To_do_list_db:
             except:
                 pass
 
-        # Envoie une requête sur tous les ports où les clients sont à l'écoute
+        #envoie une requête sur tous les ports où les clients sont à l'écoute
         for i in range(self.nbr_client):
 
-            # Utilise un thread pour éviter que le programme principal doive attendre la fin des sockets
+            #utilise un thread pour éviter que le programme principal doive attendre
+            # la fin des sockets
             thread = threading.Thread(target=update_client_end_db_thread, args=(i,), daemon=True)
             thread.start()
 
 
 
     def update_client_end_db(self):
-        """ Envois une requete à tout les clients pour prévenir que la DB à été arrêter """
+        """
+        Envois une requete à tout les clients pour prévenir que la DB à été arrêter
+        """
 
         def update_client_end_db_thread(index):
             try:
@@ -197,9 +223,10 @@ class To_do_list_db:
             except:
                 pass
 
-        # Envoie une requête sur tous les ports où les clients sont à l'écoute
+        #envoie une requête sur tous les ports où les clients sont à l'écoute
         for i in range(self.nbr_client):
 
-            # Utilise un thread pour éviter que le programme principal doive attendre la fin des sockets
+            #utilise un thread pour éviter que le programme principal doive attendre
+            # la fin des sockets
             thread = threading.Thread(target=update_client_end_db_thread, args=(i,), daemon=True)
             thread.start()
